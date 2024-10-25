8a198e4dd11def0807d7ec3f768e0ba75f9eeb72
haadiyaa <haadiyact@gmail.com>
2024-10-25 21:47:33 +0530

    patient list refresh added

--- lib/presentation/homepage/view/homepage.dart
+++ lib/presentation/homepage/view/homepage.dart
@@ -25,6 +25,12 @@ class _HomePageState extends State<HomePage> {
     functionsProvider.getPatientList();
   }
 
+  Future<void> onRefresh() async {
+    final functionsProvider =
+        Provider.of<FunctionsProvider>(context, listen: false);
+    await functionsProvider.getPatientList();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,176 +43,182 @@ class _HomePageState extends State<HomePage> {
           ),
         ],
       ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 15.0),
-            child: Column(
-              children: [
-                Row(
-                  children: [
-                    const Expanded(
-                      flex: 3,
-                      child: MySearchbar(),
-                    ),
-                    Constants.width15,
-                    Expanded(
-                      child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: AppColors.green,
-                          foregroundColor: AppColors.white,
-                          padding: const EdgeInsets.symmetric(vertical: 14),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(8),
-                          ),
-                        ),
-                        onPressed: () {},
-                        child: const Text('Search'),
+      body: RefreshIndicator(
+        onRefresh: onRefresh,
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15.0),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      const Expanded(
+                        flex: 3,
+                        child: MySearchbar(),
                       ),
-                    ),
-                  ],
-                ),
-                Constants.height20,
-                Row(
-                  children: [
-                    const Expanded(
-                      child: Text(
-                        'Sort by:',
-                        style: MyTextStyles.sort,
-                      ),
-                    ),
-                    Constants.width15,
-                    Expanded(
-                      child: Container(
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: 15,
-                          vertical: 10,
-                        ),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(30),
-                          border: Border.all(
-                            color: AppColors.black,
-                            width: 0.5,
-                          ),
-                        ),
-                        child: const Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text('Date'),
-                            Icon(
-                              Icons.keyboard_arrow_down_outlined,
-                              color: AppColors.green,
+                      Constants.width15,
+                      Expanded(
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: AppColors.green,
+                            foregroundColor: AppColors.white,
+                            padding: const EdgeInsets.symmetric(vertical: 14),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8),
                             ),
-                          ],
+                          ),
+                          onPressed: () {},
+                          child: const Text('Search'),
                         ),
                       ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ),
-          Constants.height10,
-          const Divider(),
-          Consumer<FunctionsProvider>(
-            builder: (context, value, child) => value.patientListModel != null
-                ? Expanded(
-                    flex: 7,
-                    child: ListView.separated(
-                      separatorBuilder: (context, index) => Constants.height20,
-                      itemCount: value.patientListModel!.patient.length,
-                      itemBuilder: (BuildContext context, int index) {
-                        return PatientTile(
-                          patientListModel: value.patientListModel!,
-                          index: index,
-                        );
-                      },
-                    ),
-                  )
-                : Expanded(
-                    flex: 7,
-                    child: ListView.separated(
-                      separatorBuilder: (context, index) => Constants.height20,
-                      itemCount: 3,
-                      itemBuilder: (BuildContext context, int index) {
-                        return Container(
-                          margin: const EdgeInsets.symmetric(horizontal: 20),
+                    ],
+                  ),
+                  Constants.height20,
+                  Row(
+                    children: [
+                      const Expanded(
+                        child: Text(
+                          'Sort by:',
+                          style: MyTextStyles.sort,
+                        ),
+                      ),
+                      Constants.width15,
+                      Expanded(
+                        child: Container(
                           padding: const EdgeInsets.symmetric(
-                              horizontal: 20, vertical: 15),
+                            horizontal: 15,
+                            vertical: 10,
+                          ),
                           decoration: BoxDecoration(
-                              color: AppColors.lightgrey,
-                              borderRadius: BorderRadius.circular(20)),
+                            borderRadius: BorderRadius.circular(30),
+                            border: Border.all(
+                              color: AppColors.black,
+                              width: 0.5,
+                            ),
+                          ),
                           child: const Row(
-                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
                             children: [
-                              Text('_.'),
-                              Constants.width10,
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    '___',
-                                    style: MyTextStyles.nameText,
-                                  ),
-                                  Text(
-                                    '______',
-                                    style: MyTextStyles.subNameText,
-                                  ),
-                                  Constants.height10,
-                                  Row(
-                                    children: [
-                                      Icon(
-                                        Icons.calendar_month_outlined,
-                                        color: AppColors.red,
-                                        size: 14,
-                                      ),
-                                      Constants.width5,
-                                      Text(
-                                        '__',
-                                        style: MyTextStyles.dateText,
-                                      ),
-                                      Constants.width10,
-                                      Icon(
-                                        Icons.group_outlined,
-                                        color: AppColors.red,
-                                        size: 14,
-                                      ),
-                                      Constants.width5,
-                                      Text(
-                                        '___',
-                                        style: MyTextStyles.dateText,
-                                      ),
-                                    ],
-                                  ),
-                                ],
+                              Text('Date'),
+                              Icon(
+                                Icons.keyboard_arrow_down_outlined,
+                                color: AppColors.green,
                               ),
                             ],
                           ),
-                        );
-                      },
-                    ),
+                        ),
+                      ),
+                    ],
                   ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 25),
-            child: SizedBox(
-              height: 45,
-              width: double.infinity,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: AppColors.green,
-                  foregroundColor: AppColors.white,
-                  padding: const EdgeInsets.symmetric(vertical: 14),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                ),
-                onPressed: () {},
-                child: const Text('Register'),
+                ],
               ),
             ),
-          ),
-        ],
+            Constants.height10,
+            const Divider(),
+            Consumer<FunctionsProvider>(
+              builder: (context, value, child) => value.patientListModel != null
+                  ? Expanded(
+                      flex: 7,
+                      child: ListView.separated(
+                        separatorBuilder: (context, index) =>
+                            Constants.height20,
+                        itemCount: value.patientListModel!.patient.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          return PatientTile(
+                            patientListModel: value.patientListModel!,
+                            index: index,
+                          );
+                        },
+                      ),
+                    )
+                  : Expanded(
+                      flex: 7,
+                      child: ListView.separated(
+                        separatorBuilder: (context, index) =>
+                            Constants.height20,
+                        itemCount: 3,
+                        itemBuilder: (BuildContext context, int index) {
+                          return Container(
+                            margin: const EdgeInsets.symmetric(horizontal: 20),
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 20, vertical: 15),
+                            decoration: BoxDecoration(
+                                color: AppColors.lightgrey,
+                                borderRadius: BorderRadius.circular(20)),
+                            child: const Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text('_.'),
+                                Constants.width10,
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      '___',
+                                      style: MyTextStyles.nameText,
+                                    ),
+                                    Text(
+                                      '______',
+                                      style: MyTextStyles.subNameText,
+                                    ),
+                                    Constants.height10,
+                                    Row(
+                                      children: [
+                                        Icon(
+                                          Icons.calendar_month_outlined,
+                                          color: AppColors.red,
+                                          size: 14,
+                                        ),
+                                        Constants.width5,
+                                        Text(
+                                          '__',
+                                          style: MyTextStyles.dateText,
+                                        ),
+                                        Constants.width10,
+                                        Icon(
+                                          Icons.group_outlined,
+                                          color: AppColors.red,
+                                          size: 14,
+                                        ),
+                                        Constants.width5,
+                                        Text(
+                                          '___',
+                                          style: MyTextStyles.dateText,
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 10.0, horizontal: 25),
+              child: SizedBox(
+                height: 45,
+                width: double.infinity,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: AppColors.green,
+                    foregroundColor: AppColors.white,
+                    padding: const EdgeInsets.symmetric(vertical: 14),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+                  onPressed: () {},
+                  child: const Text('Register'),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
